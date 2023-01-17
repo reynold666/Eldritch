52bd6da696b600025e6806bead3e649a58b89afc
Substain <Dav.d@gmx.de>
2023-01-17 11:52:55 +0100

    update ui and player controller

--- SCRIPTS/Player1.gd
+++ SCRIPTS/Player1.gd
@@ -4,36 +4,58 @@ extends KinematicBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-const SPEED = 50
-var MAX_SPEED = 500
 const FRICTION = 0.1
-var SPEED_BOOSTER=1
-var TOTAL_SPEED=0
+const MOVEMENT_ACCELERATION = 100
+
+export var MaximumSanity=100
+export var MaximumSpiritualEnergy=100
+export var PhaseShiftDuration=2
+export var PhaseShiftCooldown=6
+export var PowerWalkDuration=3.5
+export var PowerWalkCooldown=4
+export var TeleportCooldown=6
+export var TeleportDistance=10
+export var NormalSpeed=500
+export var PowerWalkSpeed=1200
+
+var currentSpeed = NormalSpeed
 var motion = Vector2()
-export var Sanity=100
-export var PhaseShiftDuration=0
-export var PowerWalkDuration=0
-export var PhaseShiftCooldown=0
-export var PowerWalkCooldown=0
+var Sanity=MaximumSanity
+var SpiritualEnergy=MaximumSpiritualEnergy
+
 export var InteractingWithPanel=false
+onready var playerUi = $PlayerUI
 var teleportActive = true
 var phaseShiftActive = true
 var powerWalkActive = true
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$TimerPhaseShift.wait_time=PhaseShiftDuration
+	$CooldownPhaseShift.wait_time=PhaseShiftCooldown
+	
+	$TimerPowerWalk.wait_time=PowerWalkDuration
+	$CooldownPowerWalk.wait_time=PowerWalkCooldown
+	
+	$TimerTeleport.wait_time=TeleportCooldown
+
+	#call these to make sure the UI is up to date
+	setSanity(MaximumSanity)
+	setSpiritual(MaximumSpiritualEnergy) 
+	setTeleportUsable(true)
+	setPhaseShiftUsbable(true)
+	setPowerWalkUsable(true)
+	
 	pass # Replace with function body.
 
 func _physics_process(delta):
-	updateCoolDowns()
+	updateReloadUI()
+	#updateCoolDowns()
 	if not InteractingWithPanel:
 		spectralVision()
 		if Input.is_action_just_released("PhaseShift") and phaseShiftActive:
 			PhaseShift()
 		if Input.is_action_just_released("PowerWalk") and powerWalkActive:
 			PowerWalk()
-#		print(SPEED_BOOSTER)
-#		print(MAX_SPEED)
-		ghostWalkSpeed(SPEED_BOOSTER)
 		update_movement()
 		if Input.is_mouse_button_pressed(BUTTON_LEFT) and teleportActive:
 			teleport()
@@ -41,16 +63,16 @@ func _physics_process(delta):
 	
 func update_movement():
 	if Input.is_action_pressed("Down") and not Input.is_action_pressed("Top"):
-		motion.y = clamp(motion.y + TOTAL_SPEED, 0, MAX_SPEED)
+		motion.y = clamp(motion.y + MOVEMENT_ACCELERATION, 0, currentSpeed)
 	elif Input.is_action_pressed("Top") and not Input.is_action_pressed("Down"):
-		motion.y = clamp(motion.y - TOTAL_SPEED, -MAX_SPEED, 0)
+		motion.y = clamp(motion.y - MOVEMENT_ACCELERATION, -currentSpeed, 0)
 	else:
 		motion.y = lerp(motion.y,0,FRICTION)
 		
 	if Input.is_action_pressed("Right") and not Input.is_action_pressed("Left"):
-		motion.x = clamp(motion.x + TOTAL_SPEED,0,MAX_SPEED)
+		motion.x = clamp(motion.x + MOVEMENT_ACCELERATION, 0, currentSpeed)
 	elif Input.is_action_pressed("Left") and not Input.is_action_pressed("Right"):
-		motion.x = clamp(motion.x - TOTAL_SPEED, -MAX_SPEED , 0)
+		motion.x = clamp(motion.x - MOVEMENT_ACCELERATION, -currentSpeed, 0)
 	else:
 		motion.x = lerp(motion.x,0,FRICTION)
 
@@ -76,45 +98,37 @@ func detectHidden():
 				get_tree().call_group("SignPost","makeVisible")
 				
 func updateSanity(damage):
-	Sanity=Sanity-damage
-	if Sanity==80:
-		$Light2D.texture_scale=$Light2D.texture_scale-1
-	elif Sanity==60:
-		$Light2D.texture_scale=$Light2D.texture_scale-1
-	elif Sanity==40:
-		$Light2D.texture_scale=$Light2D.texture_scale-1
-		
+	changeSanity(-damage)
+
+	
 func teleport():
 		var mousePosition = get_global_mouse_position()
 #		print(mousePosition)
 		$".".global_position=mousePosition
-		teleportActive= !teleportActive
+		setTeleportUsable(false)
+		playerUi.set_teleport_active(false)
 		$TimerTeleport.start()
 		
 func PhaseShift():
 	z_index=1
 	$PlayerSprite.modulate=Color(1,1,1,0.25)
-	phaseShiftActive= !phaseShiftActive
+	setPhaseShiftUsbable(false)
 	set_collision_mask_bit(1, false)
+	playerUi.set_phase_shift_active(false)
 	$TimerPhaseShift.start()
 
-func ghostWalkSpeed(a):
-	TOTAL_SPEED=SPEED*a
-	
 func PowerWalk():
-	SPEED_BOOSTER=5
-	MAX_SPEED=MAX_SPEED*SPEED_BOOSTER
+	currentSpeed = PowerWalkSpeed
 #	ghostWalkSpeed(SPEED_BOOSTER)
-	powerWalkActive= !powerWalkActive
+	setPowerWalkUsable(false)
+	playerUi.set_speed_active(false)
 	$TimerPowerWalk.start()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
-
 func _on_Timer_timeout():
-	teleportActive= !teleportActive
-
+	setTeleportUsable(true)
 
 func _on_TimerPhaseShift_timeout():
 	z_index=0
@@ -123,43 +137,88 @@ func _on_TimerPhaseShift_timeout():
 	$CooldownPhaseShift.start()
 	set_collision_mask_bit(1, true)
 
-
 func _on_TimerPowerWalk_timeout():
-	SPEED_BOOSTER=1
-	MAX_SPEED=500
+	currentSpeed = NormalSpeed
 	$CooldownPowerWalk.start()
 #	powerWalkActive= !powerWalkActive
 
 
 func _on_CooldownPhaseShift_timeout():
-	phaseShiftActive= !phaseShiftActive
+	setPhaseShiftUsbable(true)
 
 
 func _on_CooldownPowerWalk_timeout():
-	powerWalkActive= !powerWalkActive
+	setPowerWalkUsable(true)
 	
-func updateCoolDowns():
-	if $TimerPhaseShift.is_stopped():
-		PhaseShiftDuration=$TimerPhaseShift.wait_time
-	else:
-		PhaseShiftDuration=$TimerPhaseShift.time_left
+func updateReloadUI():
+	if $CooldownPhaseShift.time_left>0:
+		playerUi.set_phase_shift_reload(1-$CooldownPhaseShift.time_left/PhaseShiftCooldown)
 	
-	if $TimerPowerWalk.is_stopped():
-		PowerWalkDuration=$TimerPowerWalk.wait_time
-	else:
-		PowerWalkDuration=$TimerPowerWalk.time_left
+	if $CooldownPowerWalk.time_left>0:
+		playerUi.set_speed_reload(1-$CooldownPowerWalk.time_left/PowerWalkCooldown)
+	
+	if $TimerTeleport.time_left>0:
+		playerUi.set_teleport_reload(1-$TimerTeleport.time_left/TeleportCooldown)
+
+
+#func updateCoolDowns():
+	#if $TimerPhaseShift.is_stopped():
+	#	PhaseShiftDuration=$TimerPhaseShift.wait_time
+	#else:
+	#	PhaseShiftDuration=$TimerPhaseShift.time_left
+	
+	#if $TimerPowerWalk.is_stopped():
+	#	PowerWalkDuration=$TimerPowerWalk.wait_time
+	#else:
+	#	PowerWalkDuration=$TimerPowerWalk.time_left
 		
-	if $CooldownPhaseShift.is_stopped():
-		PhaseShiftCooldown=$CooldownPhaseShift.wait_time
-	else:
-		PhaseShiftCooldown=$CooldownPhaseShift.time_left
+	#if $CooldownPhaseShift.is_stopped():
+	#	PhaseShiftCooldown=$CooldownPhaseShift.wait_time
+	#else:
+	#	PhaseShiftCooldown=$CooldownPhaseShift.time_left
 	
-	if $CooldownPowerWalk.is_stopped():
-		PowerWalkCooldown=$CooldownPowerWalk.wait_time
-	else:
-		PowerWalkCooldown=$CooldownPowerWalk.time_left
+	#if $CooldownPowerWalk.is_stopped():
+	#	PowerWalkCooldown=$CooldownPowerWalk.wait_time
+	#else:
+	#	PowerWalkCooldown=$CooldownPowerWalk.time_left
+
+func setTeleportUsable(teleportUsable):
+	teleportActive = teleportUsable
+	playerUi.set_teleport_active(teleportActive)
+
+func setPhaseShiftUsbable(phaseShiftUsable):
+	phaseShiftActive = phaseShiftUsable
+	playerUi.set_phase_shift_active(phaseShiftActive)
+	
+func setPowerWalkUsable(powerWalkUsable):
+	powerWalkActive = powerWalkUsable
+	playerUi.set_speed_active(powerWalkActive)
 
 
 func interactingWithPanel():
 	InteractingWithPanel= !InteractingWithPanel
+	
+# common interface for changing sanity. e.g. to deal 50 damage, call changeSanity(-50). Also updates the UI
+func changeSanity(sanityChange):
+	setSanity(Sanity+sanityChange)
+	
+# common interface for setting the sanity directly. will be clamped between 0 and the maximum. Updates the UI accordingly.
+# also calls onZeroSanity() if the sanity goes to zero
+func setSanity(newSanity):
+	if(newSanity <= 0):
+		onZeroSanity()
+	Sanity = clamp(newSanity, 0, MaximumSanity)
+	playerUi.set_sanity_relative(float(Sanity)/MaximumSanity)
+	
+func changeSpiritual(spiritualChange):
+	setSanity(SpiritualEnergy+spiritualChange)
+	
+func setSpiritual(newSpiritual):
+	SpiritualEnergy = clamp(newSpiritual, 0, MaximumSpiritualEnergy)
+	playerUi.set_spiritual_relative(float(SpiritualEnergy)/MaximumSpiritualEnergy)
+
+func onZeroSanity():
+	# TODO: what happens here? game over screen?
+	pass
+	
 	
--- SCRIPTS/UIManager.gd
+++ SCRIPTS/UIManager.gd
@@ -1,19 +1,21 @@
-extends Control
+extends CanvasLayer
 
 onready var sanityBar = $SanityBar
 onready var spiritualBar = $SpiritualBar
-onready var teleportIconActive = $Teleport/Box
-onready var teleportIconInactive = $Teleport/Reload
-onready var speedIconActive = $Speed/Box
-onready var speedIconInactive = $Speed/Reload
-onready var phaseShiftIconActive = $PhaseShift/Box
-onready var phaseShiftIconInactive = $PhaseShift/Reload
-	
+onready var teleportIconActive = $HBox/Teleport/Box
+onready var teleportIconInactive = $HBox/Teleport/Reload
+onready var speedIconActive = $HBox/Speed/Box
+onready var speedIconInactive = $HBox/Speed/Reload
+onready var phaseShiftIconActive = $HBox/PhaseShift/Box
+onready var phaseShiftIconInactive = $HBox/PhaseShift/Reload
+onready var spectralVisionIconActive = $HBox/SpectralVision/Box
+onready var spectralVisionIconInactive = $HBox/SpectralVision/Reload
+
 func set_sanity_relative(relativeSanity):
 	sanityBar.value = relativeSanity
 
-func set_spiritual_relative(relativeSanity):
-	sanityBar.value = relativeSanity
+func set_spiritual_relative(relativeSpiritual):
+	spiritualBar.value = relativeSpiritual
 
 func set_sanity_absolute(sanityValue, sanityMaximum, sanityMinimum = 0):
 	set_sanity_relative(sanityValue-sanityMinimum/sanityMaximum-sanityMinimum)
@@ -26,9 +28,26 @@ func set_speed_active(speedIsActive):
 	speedIconInactive.visible = !speedIsActive
 		
 func set_phase_shift_active(phaseShiftIsActive):
-	speedIconActive.visible = phaseShiftIsActive
-	speedIconInactive.visible = !phaseShiftIsActive
+	phaseShiftIconActive.visible = phaseShiftIsActive
+	phaseShiftIconInactive.visible = !phaseShiftIsActive
 	
 func set_teleport_active(teleportIsActive):
 	teleportIconActive.visible = teleportIsActive
 	teleportIconInactive.visible = !teleportIsActive
+	
+func set_spectral_vision_active(spectralVisionIsActive):
+	spectralVisionIconActive.visible = spectralVisionIsActive
+	spectralVisionIconInactive.visible = !spectralVisionIsActive
+	
+	
+func set_speed_reload(relSpeedReload):
+	speedIconInactive.value = relSpeedReload
+		
+func set_phase_shift_reload(relPhaseShiftReload):
+	phaseShiftIconInactive.value = relPhaseShiftReload
+	
+func set_teleport_reload(relTeleportReload):
+	teleportIconInactive.value = relTeleportReload
+	
+func set_spectral_vision_reload(relSpectralVisionReload):
+	spectralVisionIconInactive.value = relSpectralVisionReload
